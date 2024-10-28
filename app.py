8a92ae7e37aa873660255bb39bda24ea50393172
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-10-28 13:55:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ if __name__ == "__main__":
     app = create_app()
     app.run(debug=True,
             host="0.0.0.0",
-            port=int(os.environ.get("PORT", 8080)))
+            port=int(os.environ.get("PORT", 6835)))
